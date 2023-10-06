af2948346bd02f882c467dbaaef156165e8ec4be
whysobad <93646171+learnjavalorant@users.noreply.github.com>
2023-10-06 22:53:46 +0700

    Tải FILE!

---
+++ src/main/java/vietnam/tansenpai/dummypluggay/main/DummyPlugGay.java
@@ -0,0 +1,12 @@
+package vietnam.tansenpai.dummypluggay.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DummyPlugGay
+extends JavaPlugin {
+    public void onEnable() {
+    }
+
+    public void onDisable() {
+    }
+}
